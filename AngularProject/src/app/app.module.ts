38eb9eee82543f77e13e6c94d0adbd5d9b85a6de
sumuongit <sumu2007@gmail.com>
2019-12-09 13:23:37 +0600

    This is first commit

---
+++ AngularProject/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { CustomerComponent } from './customers/customer/customer.component';
+import { CustomerListComponent } from './customers/customer-list/customer-list.component';
+import { CustomerService } from './shared/customer.service';
+import { FormsModule } from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomersComponent,
+    CustomerComponent,
+    CustomerListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [CustomerService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ AngularProject/src/app/shared/customer.model.ts
@@ -0,0 +1,7 @@
+export class Customer {
+    CustomerId: number;
+    FullName: string;
+    CustomerCode: string;
+    MobileNo: string;
+    Address: string;
+}
